5d5eb25403c6fd283101cba490adf50b57782c94
ss-komori <ss.komori53@gmail.com>
2024-01-29 17:09:08 +0900

    useSWRの初期値がundefinedだった場合にinitialDataをセットするように修正

--- src/features/hooks/useStaticSWR.tsx
+++ src/features/hooks/useStaticSWR.tsx
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
 import useSWR from "swr";
 
 async function fetcher(key: string, init?: RequestInit) {
   return fetch(key, init).then((res) => res.json());
 }
 
-export const useStaticSWR = <T,>(key: string) => {
-  const { data, mutate } = useSWR<T>(key, fetcher);
+export const useStaticSWR = <T,>(key: string, initialData: T) => {
+  const { data = initialData, mutate } = useSWR<T>(key, fetcher, {
+    fallbackData: initialData,
+  });
+
+  useEffect(() => {
+    // swrでキャッシュされているdataがundefinedだった場合に、initialDataをset
+    mutate((_data) => _data || initialData, false);
+  }, []);
 
   return { data, mutate };
 };
--- src/features/hooks/useStatickTask.tsx
+++ src/features/hooks/useStatickTask.tsx
@@ -3,7 +3,7 @@ import { useStaticSWR } from "./useStaticSWR";
 import { Task } from "@/types";
 
 export const useStaticTask = () => {
-  const { data: tasks, mutate } = useStaticSWR<Task[]>("/api/task");
+  const { data: tasks, mutate } = useStaticSWR<Task[]>("/api/task", []);
 
   return {
     tasks,
