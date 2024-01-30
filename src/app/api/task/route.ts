3dd03c5a24244f6ce26c2f35d55a2a3f35ae5a47
ss-komori <ss.komori53@gmail.com>
2024-01-30 16:52:30 +0900

    fetchからaxiosに変更

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,17 +1,21 @@
 import { Task } from "@/types";
 import { NextRequest, NextResponse } from "next/server";
 import { number, z } from "zod";
+import axios from "axios";
+
+const url = "http://backend:80/api/tasks";
+// モックサーバー
+// const url = "http://host.docker.internal:3658/m1/448579-0-default/api/tasks";
 
 export async function GET() {
   try {
-    const res = await fetch(`http://backend:80/api/tasks`).then((res) =>
-      res.json()
-    );
-    if (!res || Object.keys(res).length === 0) {
+    const res = await axios.get(url);
+
+    if (!res.data || Object.keys(res).length === 0) {
       return NextResponse.json([], { status: 500 });
     }
 
-    return NextResponse.json(res, { status: 201 });
+    return NextResponse.json(res.data, { status: 201 });
   } catch (error) {
     return NextResponse.json([], { status: 500 });
   }
@@ -22,43 +26,36 @@ export async function POST(request: NextRequest) {
 
   const task = result.task as Task;
 
-  const res = await fetch(`http://backend:80/api/tasks`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
+  try {
+    const res = await axios.post(url, {
       title: task.title,
       content: task.content,
       status: task.status,
-    }),
-  }).then((res) => res.json());
+    });
 
-  console.log("res", res);
-  return NextResponse.json(res, { status: 200 });
+    return NextResponse.json(res.data, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json([], { status: 500 });
+  }
 }
 
 export async function DELETE(request: NextRequest) {
   const result = await request.json();
   const id = result.id;
 
-  const res = await fetch(`http://backend:80/api/tasks/${id}`, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
-  });
-  return NextResponse.json(res, { status: 200 });
+  const res = await axios.delete(`${url}/${id}`);
+  return NextResponse.json(res.data, { status: 200 });
 }
 export async function PATCH(request: NextRequest) {
   const result = await request.json();
   const task = result.task as Task;
 
-  const res = await fetch(`http://backend:80/api/tasks/${task.id}`, {
-    method: "PATCH",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      id: task.id,
-      title: task.title,
-      content: task.content,
-      status: task.status,
-    }),
+  const res = await axios.patch(`${url}/${task.id}`, {
+    id: task.id,
+    title: task.title,
+    content: task.content,
+    status: task.status,
   });
-  return NextResponse.json(res, { status: 200 });
+  return NextResponse.json(res.data, { status: 200 });
 }
